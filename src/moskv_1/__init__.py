413195ed42594307d72e548c3a56d5a7c1aff45e
Borja Moskv <82632764+borjamoskv@users.noreply.github.com>
2026-06-18 01:01:51 +0200

    feat(core): implement specialized SuperAgent classes for the executive board

--- src/moskv_1/__init__.py
+++ src/moskv_1/__init__.py
@@ -8,6 +8,14 @@ from moskv_1.event_bus import EventBus, CortexEvent
 from moskv_1.brain import BrainRegion
 from moskv_1.memory import MemoryStore
 from moskv_1.immunity import ImmunityLayer, ImmuneState
+from moskv_1.super_agents import (
+    IntelligenceAgent,
+    MarketingSyndicatorAgent,
+    MarketingOutreachAgent,
+    ComplianceAgent,
+    InfrastructureAgent,
+    FinanceAgent
+)
 
 __all__ = [
     "EventBus",
@@ -16,5 +24,11 @@ __all__ = [
     "MemoryStore",
     "ImmunityLayer",
     "ImmuneState",
+    "IntelligenceAgent",
+    "MarketingSyndicatorAgent",
+    "MarketingOutreachAgent",
+    "ComplianceAgent",
+    "InfrastructureAgent",
+    "FinanceAgent",
 ]
 
